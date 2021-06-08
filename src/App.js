51d35156a3ea524ba672604bcac0ce519a384f4d
Your Name <you@example.com>
2021-06-09 00:37:56 +0800

    home

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.scss';
 import {HashRouter, Switch, Route} from "react-router-dom"
 import Login from './pages/login'
+import Home from './pages/home'
 
 function App() {
-  return (
-    <div className="App">
-      <HashRouter>
-        <Switch>
-          <Route path="/login" component={Login}></Route>
-        </Switch>
-      </HashRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HashRouter>
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/" component={Home}></Route>
+                </Switch>
+            </HashRouter>
+        </div>
+    );
 }
 
 export default App;
