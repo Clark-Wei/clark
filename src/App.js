633358ce1a9bfe504f6cb30f20fe472dfbb836d2
xsl-git01 <52912079+xsl-git01@users.noreply.github.com>
2021-06-09 11:01:26 +0800

    remove url #

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.scss';
-import {HashRouter, Switch, Route} from "react-router-dom"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Login from './pages/login'
 import Home from './pages/home'
 
 function App() {
     return (
         <div className="App">
-            <HashRouter>
+            <BrowserRouter>
                 <Switch>
                     <Route path="/login" component={Login}></Route>
                     <Route path="/" component={Home}></Route>
                 </Switch>
-            </HashRouter>
+            </BrowserRouter>
         </div>
     );
 }
