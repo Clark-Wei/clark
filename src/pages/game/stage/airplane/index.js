abd5750a54847f2456cba452a0a5eed91ef44ac7
Clark-Wei <65336449+Clark-Wei@users.noreply.github.com>
2021-09-07 00:21:21 +0800

    dev

--- src/pages/game/stage/airplane/index.js
+++ src/pages/game/stage/airplane/index.js
@@ -1,18 +1,22 @@
 import React, {useEffect, useState} from 'react'
 import "./style.scss"
+import {CaretRightOutlined} from '@ant-design/icons';
 
 function Airplane(props) {
+    let [game, setGame] = useState({  // 游戏类
+        start: false,   // 游戏开始
+    })
 
     useEffect(() => {
-
-    },[])
-
+        console.log(game.start)
+    }, [])
 
 
     return (
         <div className="Airplane">
-
-
+            <div className="start-button">
+                <CaretRightOutlined/>
+            </div>
         </div>
     );
 }
