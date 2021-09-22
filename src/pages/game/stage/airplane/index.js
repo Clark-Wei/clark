f8bbe2446305b761a6df392742bf9739e53b8fe4
Clark-Wei <65336449+Clark-Wei@users.noreply.github.com>
2021-09-22 12:22:10 +0800

    feiji

--- src/pages/game/stage/airplane/index.js
+++ src/pages/game/stage/airplane/index.js
@@ -5,13 +5,16 @@ import heroImg from '../../../components/img/airplaneImgs/hero.png';
 
 function Airplane(props) {
     let hero = useRef(null) // 我机
+    let bg = useRef(null) // 背景
 
     let [game, setGame] = useState({     // 游戏类
         start: false,   // 游戏开始
         point: 0,   // 积分
     })
     let [mine, setMine] = useState({     // 我机类
-        img: heroImg
+        img: heroImg,
+        x: 0,
+        y: 0,
     })
     let [enemyPlane, setEnemyPlane] = useState({     // 敌机类
     })
@@ -31,38 +34,51 @@ function Airplane(props) {
         setGame({...game})
 
         setTimeout(() => {
-            window.addEventListener('keydown', (e) => {
-                console.log(e.keyCode)
-                switch (e.keyCode) {
-                    case 37:
-                        keyMove('left');
-                        break
-                    case 38:
-                        keyMove('top');
-                        break
-                    case 39:
-                        keyMove('right');
-                        break
-                    case 40:
-                        keyMove('bottom');
-                        break
-                }
-            })
-        }, 1000)
+            mine.x = bg.current.scrollWidth / 2 - hero.current.width / 2
+            mine.y = bg.current.scrollHeight - hero.current.height
+            setMine({...mine})
+        }, 0)
     }
-    let keyMove = (type) => {
-        console.log(hero.current)
+    let checkBeyond = (type) => {
+        window.addEventListener('keydown', (e) => {
+            console.log(e.keyCode)
+
+            switch (e.keyCode) {
+                case 37:
+                    if (mine.x - 20 < 0) return
+                    mine.x -= 20
+                    setMine({...mine})
+                    break
+                case 38:
+                    if (mine.y - 20 < 0) return
+                    mine.y -= 20
+                    setMine({...mine})
+                    break
+                case 39:
+                    if (mine.x + 20 > bg.current.scrollWidth - hero.current.width) return
+                    mine.x += 20
+                    setMine({...mine})
+                    break
+                case 40:
+                    if (mine.y + 20 > bg.current.scrollHeight - hero.current.height) return
+                    mine.y += 20
+                    setMine({...mine})
+                    break
+            }
+        })
     }
 
     return (
-        <div className="Airplane">
+        <div className="Airplane" ref={bg}>
             <div className="point-div">分数: <span className="point-num">{game.point}</span></div>
             {!game.start ? (
                 <div className="start-button" onClick={start}>
                     <CaretRightOutlined/>
                 </div>
             ) : ''}
-            <img src={mine.img} className={'hero-img ' + (game.start ? 'show' : '')} ref={hero}/>
+            <img src={mine.img}
+                 style={{top: mine.y, left: mine.x}}
+                 className={'hero-img ' + (game.start ? 'show' : '')} ref={hero}/>
         </div>
     );
 }
